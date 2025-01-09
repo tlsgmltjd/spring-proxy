e9e38022cec44c3ebb41c5108f1a51baddd169ca
신희성 <vvwv3092@gmail.com>
2025-01-09 14:07:43 +0900

    add: interface + impl v1

---
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -0,0 +1,14 @@
+package hello.proxy.app.v1;
+
+import org.springframework.web.bind.annotation.*;
+
+//@RequestMapping // 스프링은 @Controller or @RequestMapping이 있으면 스프링 컨트롤러로 인식 -> 3.0 부터 @RestController 붙혀야함
+@RestController
+@ResponseBody
+public interface OrderControllerV1 {
+    @GetMapping("/v1/request")
+    String request(@RequestParam("itemId") String itemId);
+
+    @GetMapping("/v1/no-log")
+    String noLog();
+}
