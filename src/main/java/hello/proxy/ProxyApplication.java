caeb8e66a5ae2950e775a3025083ca2a35a82975
신희성 <vvwv3092@gmail.com>
2025-01-12 16:31:28 +0900

    add: jdk filter dynamic proxy

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,7 @@ package hello.proxy;
 
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -17,7 +18,8 @@ import org.springframework.context.annotation.Import;
 //@Import(AppV1Config.class)
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import({ConcreteProxyConfig.class})
-@Import({DynamicProxyBasicConfig.class, AppV2Config.class})
+//@Import({DynamicProxyBasicConfig.class, AppV2Config.class})
+@Import({DynamicProxyFilterConfig.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
