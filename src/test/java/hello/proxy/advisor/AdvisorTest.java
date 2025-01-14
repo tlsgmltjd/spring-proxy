f4c8f329e57a9ce5086439072c4ef4508e5223b0
신희성 <vvwv3092@gmail.com>
2025-01-14 20:03:34 +0900

    add: advisor[advice, pointcut] example

---
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,25 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+class AdvisorTest {
+
+    @Test
+    void advisorTest1() {
+        ServiceImpl service = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(service);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.find();
+        proxy.save();
+
+    }
+}
