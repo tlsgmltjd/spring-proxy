2b06ff10a6927fe3beab6c4c31642ce24a09785a
신희성 <vvwv3092@gmail.com>
2025-02-05 16:01:42 +0900

    add: @Aspect description

--- src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
+++ src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -8,6 +8,13 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
 @Slf4j
+
+// ++ 추가정보: 스프링은 빈이 등록될 때 해당 빈이 프록시 적용 대상인지 확인하기 위해 빈 후처리기와 비교 대상인 어드바이저 빈 안에 있는 포인트 컷을 확인해야한다.
+// 그렇다면 어드바이저 빈은 다른 빈 객체보다 우선 등록 되어야 하는데, 스프링 내부에서 빈 등록 우선순위를 두어 이 문제를 해결한다.
+
+// AnnotationAwareAspectJAutoProxyCreator 는 @Aspect 어노테이션을 찾아 Advior로 만들어준다.
+// 이 자동 프록시 생성기는 @Aspect 어노테이션이 붙은 스프링 빈을 어드바이저로 변환하여 저장하고, 어드바이저 기반으로 프록시 객체를 생성하는 두가지 일을 한다.
+// -> 이렇게 만들어잔 어드바이저들은 내부 어드바이저 빌더에 저장 해두고 자동 프록시 생성기가 스프링 빈이 등록 될 때 스플이 컨테이너에서 등록되어있는 어드바이저 빈을 조회해서 포인트컷을 확인 할때 같이 확인된다.
 @Aspect // 어노테이션 기반 프록시를 적용할 때 사용한다.
 public class LogTraceAspect {
 
